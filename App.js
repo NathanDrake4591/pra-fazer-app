fc10263310681b159f501bf51d397ab1d6044c48
NathanDrake4591 <107649020+NathanDrake4591@users.noreply.github.com>
2024-03-31 02:09:59 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
 
 export default function App() {
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image style={styles.imagemLogo} source={require('../../atividades/pra-fazer-app/assets/logo.png')}></Image>
+      <TextInput style={styles.input} placeholder='E-mail'></TextInput>
+      <TextInput style={styles.input} placeholder='Senha'></TextInput>
+      <TouchableOpacity style={styles.botaoentrar}>
+        <Text style={styles.botaoentrartexto}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.botaocriar}>
+        <Text style={styles.botaocriartexto}>Criar Usuário</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +23,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF6600',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input:{
+    backgroundColor: '#FFF',
+    width: '80%',
+    height: 70,
+    marginTop: 25,
+    borderRadius: 10,
+    paddingLeft: 25,
+  },
+
+  botaoentrar:{
+    backgroundColor: '#070A52',
+    width:'80%',
+    height: 55,
+    marginTop: 25,
+    borderRadius: 10,
+  },
+
+  botaoentrartexto:{
+    color:'#FFF',
+    textAlign: 'center',
+    fontSize: 25,
+    padding: 10,
+  },
+
+  botaocriar:{
+    marginTop: 25,
+  },
+
+  botaocriartexto:{
+    color: '#FFF',
+  }
+  
 });
